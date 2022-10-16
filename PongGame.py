bd12774f3378559753e0d239ac3039d409fb0647
Giovanni <gc3kyp@virginia.edu>
2022-10-16 17:30:04 -0400

    first gio commit

--- PongGame.py
+++ PongGame.py
@@ -5,7 +5,7 @@ from TeamPlayer import Team, Player
 class PongGame:
     def __init__(self):
         self.playerA1 = "ANTHONY" #input("Enter first player's name on Team A: ").upper()
-        self.playerA2 = "ANTHONY" #input("Enter second player's name on Team A (input same name for singles): ").upper()
+        self.playerA2 = "GIO" #input("Enter second player's name on Team A (input same name for singles): ").upper()
         self.teamA = Team(self.playerA1, self.playerA2)
         self.playerB1 = "THOMAS" #input("Enter first player's name on Team B: ").upper()
         self.playerB2 = "BEN" #input("Enter second player's name on Team B (input same name for singles): ").upper()
